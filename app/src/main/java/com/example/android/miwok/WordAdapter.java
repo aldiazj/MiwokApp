ed1bcfe9683348ff6b428fb0b50357af4fd3e8aa
alejandrodiazjllo <alejandrodiazjllo@gmail.com>
2018-03-21 21:55:36 -0500

    Adds Content to the lists of words activities

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,47 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    public WordAdapter(Activity context, ArrayList<Word> words){
+        super(context, 0, words);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link Word} object located at this position in the list
+        Word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the default translation
+        TextView defaultTranslationTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        // Get the version name from the current AndroidFlavor object and
+        // set this text on the name TextView
+        defaultTranslationTextView.setText(currentWord.getDefaultTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID version_number
+        TextView miwokTranslationTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        // Get the version number from the current AndroidFlavor object and
+        // set this text on the number TextView
+        miwokTranslationTextView.setText(currentWord.getMiwokTranslation());
+        return listItemView;
+    }
+}
